3d36453778cd1b3cf6b68de87bd2b79fa1cfe9a8
Nikita Tyukov <tukov@bk.ru>
2024-12-26 06:47:23 +0700

    first

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,6 +16,19 @@ class HorseTest {
         });
         assertEquals("Name cannot be null.", exception.getMessage());
     }
+
+    @DisplayName("Тест конструктора Horse с первым параметром пробелы, табуляция...")
+    @ParameterizedTest
+    @ValueSource(strings = {"   ", "\\t", " "})
+    public void testConstructorWithEmptyName(String word){
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Horse(word, 25, 3);
+        });
+        assertEquals("Name cannot be blank.", exception.getMessage());
+    }
+
+
 
     @Test
     void getName() {
