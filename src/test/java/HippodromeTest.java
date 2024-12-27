42dd78152ce5f7bc049417488053e1744a0637cd
Nikita Tyukov <tukov@bk.ru>
2024-12-27 08:08:36 +0700

    Hippodrome 75% finished

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,10 +1,13 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mockStatic;
 
 class HippodromeTest {
 
@@ -19,7 +22,7 @@ class HippodromeTest {
     }
 
     @Test
-    @DisplayName("при передаче в конструктор пустого списка, выброшенное исключение будет содержать сообщение \"Horses cannot be empty.\";")
+    @DisplayName("при передаче в конструктор пустого списка, выброшенное исключение будет содержать сообщение Horses cannot be empty")
     public void testConstructorWithEmptyList(){
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
@@ -29,19 +32,34 @@ class HippodromeTest {
     }
 
     @Test
+    @DisplayName("метод возвращает список, который содержит те же объекты и в той же последовательности, что и список который был передан в конструктор.")
     void getHorses() {
         List<Horse> horses = new ArrayList<>();
-        for (int i = 0; i < 50; i++) {
-            horses.add(new Horse("Horse "+i, i, i /2));
+        for (int i = 0; i < 30; i++) {
+            horses.add(new Horse("Horse "+i, i, i / 2));
         }
 
         Hippodrome hippodrome = new Hippodrome(horses);
 
-        assertEquals(horses, hippodrome.getHorses());
+        assertIterableEquals(horses, hippodrome.getHorses());
     }
 
     @Test
+    @DisplayName("Проверить, что метод вызывает метод move у всех лошадей. ")
     void move() {
+        List<Horse> horses = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            horses.add(Mockito.spy(new Horse("Name", 25)));
+        }
+
+        Hippodrome hippodrome = Mockito.spy(new Hippodrome(horses));
+
+        hippodrome.move();
+        for (Horse horse : horses) {
+            Mockito.verify(horse).move();
+        }
+
+
     }
 
     @Test
